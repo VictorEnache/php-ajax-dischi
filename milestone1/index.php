01c25f8790dee242bd1a73ecee57c5bdbee7b066
VictorEnache <victor22enache@gmail.com>
2022-05-24 16:24:39 +0200

    stampare in pagina le immagini

--- milestone1/index.php
+++ milestone1/index.php
@@ -83,3 +83,25 @@ al caricamento della pagina ci saranno tutti i dischi.
 
    var_dump($dischi[0]->poster)
  ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php foreach($dischi as $disco) : ?>
+        <div class="card">
+            <div class="image">
+                <img src="<?php echo $disco->poster ?>" alt="">
+            </div>
+        </div>
+    <?php endforeach; ?>
+
+</body>
+</html>
