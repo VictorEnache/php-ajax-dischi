f095986278e816b0fbfb14455d822c6faf0e12f8
VictorEnache <victor22enache@gmail.com>
2022-05-24 18:37:08 +0200

    stile Vuejs

--- milestone2/dischi/src/App.vue
+++ milestone2/dischi/src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="card" :key="index" v-for="(disco, index) in dischi">
+          <div class="image">
+            <img :src="disco.poster" alt="">
+          </div>
+          <div class="text">
+            <h2>{{disco.title}}</h2>
+            <div class="author">
+              {{disco.author}}
+            </div>
+            <div class="year">
+              {{disco.year}}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
+
+  data() {
+    return{
+      dischi: []
+    }
+  },
+
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+
+  mounted () {
+    axios.get('http://localhost:8888/PHP/boolean/php-ajax-dischi/milestone2/php/')
+        .then((oggetto) => {
+          this.dischi = oggetto.data
+          console.log(oggetto.data);
+      });
   }
 }
 </script>
 
 <style lang="scss">
+@import './assets/scss/style.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
